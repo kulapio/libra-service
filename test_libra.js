2634bc72032fe7b5f11f4fbe4a155bea20608734
Nattapon Nimakul <totizy@gmail.com>
2019-06-20 15:06:15 +0700

    add createAccount func

--- test_libra.js
+++ test_libra.js
@@ -9,25 +9,26 @@ const sleep = (milliseconds) => {
 
 class Libra {
   constructor () {
-
+    this.userAddress = ''
+    this.balance = ''
   }
 
-  async main() {
-    // /Users/totiz/libra
-    // /Users/totiz/Desktop/testjs
-    // docker run --rm -it thanandorn/libra_client
-    // const source = spawn('/Users/totiz/libra/scripts/cli/start_cli_testnet.sh', [],
+  async createAccount() {
     const source = spawn('docker', ['run', '-v' , walelt_path + ':/wallet', '--rm', '-i', 'thanandorn/libra_client'],
-      {stdio: ['pipe', 'pipe', process.stderr]}); // (A)
+      {stdio: ['pipe', 'pipe', process.stderr]});
   
-    this.writeToWritable(source.stdin); // (B)
-    await this.echoReadable(source.stdout); // (B)
+    this.createAccountWriteToWritable(source.stdin);
+    await this.createAccountReadable(source.stdout);
     // await onExit(source);
   
-    console.log('### DONE');
+    // console.log('### DONE');
+    return {
+      address: this.userAddress,
+      balance: this.balance
+    }
   }
   
-  async writeToWritable(writable) {
+  async createAccountWriteToWritable(writable) {
     await sleep(2000)
     await streamWrite(writable, 'account create\n');
     await sleep(1000)
@@ -45,18 +46,31 @@ class Libra {
     // await streamEnd(writable);
   }
   
-  async echoReadable(readable) {
+  async createAccountReadable(readable) {
     for await (const line of chunksToLinesAsync(readable)) { // (C)
       if (-1 != line.search("Created/retrieved account #0")) {
-        let address = line.split('account #0 address ')[1]
-        console.log('Your address: ' + address)
+        this.userAddress = line.split('account #0 address ')[1].replace('\n', '')
+        console.log('Your address: ' + this.userAddress)
       } else if (-1 != line.search("Too Many Requests")) {
         console.error('Too Many Requests: ', line)
+      } else if (-1 != line.search("Balance is: ")) {
+        this.balance = line.split('Balance is: ')[1].replace('\n', '')
+        console.log('Your balance: ' + this.balance)
       }
       console.log('LINE: '+chomp(line))
     }
   }  
 }
 
-let libra = new Libra()
-libra.main()
+
+
+(async () => {
+  try {
+    let libra = new Libra()
+    let wallet = await libra.createAccount()
+    console.log('wallet', wallet)
+
+  } catch (error) {
+    logger.error(error)
+  }
+})()
