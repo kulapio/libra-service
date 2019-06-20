680f603e5101c62578b62c2c6528d7e5b8b7b353
Nattapon Nimakul <totizy@gmail.com>
2019-06-20 14:58:49 +0700

    make Libra class

--- test_libra.js
+++ test_libra.js
@@ -7,49 +7,56 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-async function main() {
-  // /Users/totiz/libra
-  // /Users/totiz/Desktop/testjs
-  // docker run --rm -it thanandorn/libra_client
-  // const source = spawn('/Users/totiz/libra/scripts/cli/start_cli_testnet.sh', [],
-  const source = spawn('docker', ['run', '-v' , walelt_path + ':/wallet', '--rm', '-i', 'thanandorn/libra_client'],
-    {stdio: ['pipe', 'pipe', process.stderr]}); // (A)
+class Libra {
+  constructor () {
 
-  writeToWritable(source.stdin); // (B)
-  await echoReadable(source.stdout); // (B)
-  // await onExit(source);
-
-  console.log('### DONE');
-}
-main();
-
-async function writeToWritable(writable) {
-  await sleep(2000)
-  await streamWrite(writable, 'account create\n');
-  await sleep(1000)
-  await streamWrite(writable, 'account mint 0 100\n');
-  await sleep(1000)
-  await streamWrite(writable, 'account list\n');
-  await sleep(1000)
-  await streamWrite(writable, `query balance 0\n`);
-  await sleep(1000)
-  await streamWrite(writable, 'quit\n');
-  // await streamWrite(writable, 'First line\n');
-  // await sleep(1000)
-  // await streamWrite(writable, 'Second line\n');
-  // await sleep(1000)
-  // await streamEnd(writable);
-}
-
-async function echoReadable(readable) {
-  for await (const line of chunksToLinesAsync(readable)) { // (C)
-    if (-1 != line.search("Created/retrieved account #0")) {
-      let address = line.split('account #0 address ')[1]
-      console.log('Your address: ' + address)
-    } else if (-1 != line.search("Too Many Requests")) {
-      console.error('Too Many Requests: ', line)
-    }
-    console.log('LINE: '+chomp(line))
   }
+
+  async main() {
+    // /Users/totiz/libra
+    // /Users/totiz/Desktop/testjs
+    // docker run --rm -it thanandorn/libra_client
+    // const source = spawn('/Users/totiz/libra/scripts/cli/start_cli_testnet.sh', [],
+    const source = spawn('docker', ['run', '-v' , walelt_path + ':/wallet', '--rm', '-i', 'thanandorn/libra_client'],
+      {stdio: ['pipe', 'pipe', process.stderr]}); // (A)
+  
+    this.writeToWritable(source.stdin); // (B)
+    await this.echoReadable(source.stdout); // (B)
+    // await onExit(source);
+  
+    console.log('### DONE');
+  }
+  
+  async writeToWritable(writable) {
+    await sleep(2000)
+    await streamWrite(writable, 'account create\n');
+    await sleep(1000)
+    await streamWrite(writable, 'account mint 0 100\n');
+    await sleep(1000)
+    await streamWrite(writable, 'account list\n');
+    await sleep(1000)
+    await streamWrite(writable, `query balance 0\n`);
+    await sleep(1000)
+    await streamWrite(writable, 'quit\n');
+    // await streamWrite(writable, 'First line\n');
+    // await sleep(1000)
+    // await streamWrite(writable, 'Second line\n');
+    // await sleep(1000)
+    // await streamEnd(writable);
+  }
+  
+  async echoReadable(readable) {
+    for await (const line of chunksToLinesAsync(readable)) { // (C)
+      if (-1 != line.search("Created/retrieved account #0")) {
+        let address = line.split('account #0 address ')[1]
+        console.log('Your address: ' + address)
+      } else if (-1 != line.search("Too Many Requests")) {
+        console.error('Too Many Requests: ', line)
+      }
+      console.log('LINE: '+chomp(line))
+    }
+  }  
 }
 
+let libra = new Libra()
+libra.main()
