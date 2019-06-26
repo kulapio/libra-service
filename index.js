4cffea98134163eac83ec75a5cc9fa018962a8bb
Suraneti <suraneti.rod@gmail.com>
2019-06-26 11:36:42 +0700

    Served service with dynamic config env

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-var express = require('express');
-const bodyParser = require('body-parser');
-var app = express();
-const PORT = 3000
+require('dotenv').config()
+
+const bodyParser = require('body-parser')
+const express = require('express')
 const Libra = require('./libra_service.js')
 
+const app = express()
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || 'localhost'
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const libra = new Libra()
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/hello', function(req, res){
-   res.send("Hello World!");
-});
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
-app.post('/createWallet', async function(req, res){
+app.get('/hello', function (req, res) {
+  res.send("Hello World!")
+})
+
+app.post('/createWallet', async function (req, res) {
   console.log('req body', req.body)
-  let libra = new Libra()
   let wallet = await libra.createAccount()
   console.log('wallet', wallet)
-   res.send(wallet);
-});
+  res.send(wallet)
+})
 
-app.post('/getBalance', async function(req, res){
+app.post('/getBalance', async function (req, res) {
   console.log('req body', req.body)
   let address = req.body.address
-  let libra = new Libra()
   let wallet = await libra.getBalance(address)
   console.log('wallet', wallet)
-   res.send(wallet);
-});
+  res.send(wallet)
+})
 
-app.post('/transfer', async function(req, res){
+app.post('/transfer', async function (req, res) {
   console.log('req body', req.body)
   let fromAddress = req.body.fromAddress
   let mnemonic = req.body.mnemonic
   let toAddress = req.body.toAddress
   let amount = req.body.amount
-  let libra = new Libra()
   let wallet = await libra.transfer(fromAddress, mnemonic, toAddress, amount)
   console.log('wallet', wallet)
-   res.send(wallet);
-});
+  res.send(wallet)
+})
 
-
-app.listen(PORT, () => {
-  console.log('Server is running on PORT:',PORT);
-});
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on ${HOST} PORT: ${PORT}`)
+})
