b38cf18a873656c76d3211f336b1d71721542618
Suraneti <suraneti.rod@gmail.com>
2019-08-20 20:23:26 +0700

    Changed method from GET to POST

--- service/faucet.js
+++ service/faucet.js
@@ -1,17 +1,14 @@
-const axios = require('axios');
-const BigNumber = require('bignumber.js');
+const axios = require('axios')
+const BigNumber = require('bignumber.js')
 
 class Faucet {
-  constructor () {
-  }
-
   async getFaucetFromLibraTestnet(amount, address) {
     // Convert to micro libras
     const amountInMicro = BigNumber(amount).times(1e6)
     const url = `http://faucet.testnet.libra.org?amount=${amountInMicro.toString(10)}&address=${address}`
     console.log(`calling faucet ${url}`)
 
-    const response = await axios.get(url);
+    const response = await axios.post(url)
     return response
   }
 
@@ -21,7 +18,7 @@ class Faucet {
     const url = `https://libraservice3.kulap.io/faucet?amount=${amountInMicro.toString(10)}&address=${address}`
     console.log(`calling faucet ${url}`)
 
-    const response = await axios.get(url);
+    const response = await axios.get(url)
     return response
   }
 }
